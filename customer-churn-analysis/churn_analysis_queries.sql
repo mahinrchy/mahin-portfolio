0c5a05f3b5423a4ab722cd7bb11939a81302faa9
Mahin Chowdhury <mahin.mrc@gmail.com>
2026-05-05 21:04:31 -0400

    Add files via upload

---
+++ customer-churn-analysis/churn_analysis_queries.sql
@@ -0,0 +1,42 @@
+-- Churn Rate by Contract
+
+SELECT 
+    Contract,
+    COUNT(*) AS total_customers,
+    SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) AS churned_customers,
+    ROUND(
+        SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 
+        2
+    ) AS churn_rate
+FROM churn_data
+GROUP BY Contract
+ORDER BY churn_rate DESC;
+
+-- Churn Rate by Payment Method
+
+SELECT 
+    PaymentMethod,
+    COUNT(*) AS total_customers,
+    SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) AS churned_customers,
+    ROUND(
+        SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 
+        2
+    ) AS churn_rate
+FROM churn_data
+GROUP BY PaymentMethod
+ORDER BY churn_rate DESC;
+
+-- Churn by Tenure (Early Customers Risk)
+
+SELECT 
+    tenure,
+    COUNT(*) AS total_customers,
+    SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) AS churned_customers,
+    ROUND(
+        SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 
+        2
+    ) AS churn_rate
+FROM churn_data
+GROUP BY tenure
+ORDER BY tenure;
+
